3db245b74b5b395fd640b6cf99354aeca80fc948
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-07-07 09:56:29 +0700

    update: home screen client

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test_mlkit_app/home_screen.dart';
+import 'package:flutter_test_mlkit_app/home_screen_client.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -11,6 +12,11 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: "Flutter Test Liveness", debugShowCheckedModeBanner: false, home: HomeScreen(motionCount: 3));
+    return const MaterialApp(
+      title: "Flutter Test Liveness",
+      debugShowCheckedModeBanner: false,
+      home: HomeScreenClient(motionCount: 3),
+      // home: HomeScreen(motionCount: 3),
+    );
   }
 }
